95ab76cc49c6a288fa6b63c0a246ae33c4fd1624
John-son Hippolyte <Hippolyte.Johnson@ac-paris.fr>
2021-12-27 13:58:28 +0100

    generate generator

--- src/app/program-generator/program-generator.module.ts
+++ src/app/program-generator/program-generator.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputProgramComponent } from './input-program/input-program.component';
+import { GeneratorComponent } from './generator/generator.component';
 
 
 
 @NgModule({
   declarations: [
-    InputProgramComponent
+    InputProgramComponent,
+    GeneratorComponent
   ],
   imports: [
     CommonModule
