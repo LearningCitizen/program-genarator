1a2f8a9914f06520e62d1caaf1974426b117ec2b
John-son Hippolyte <Hippolyte.Johnson@ac-paris.fr>
2021-12-27 13:57:02 +0100

    generate input program

---
+++ src/app/program-generator/input-program/input-program.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-program',
+  templateUrl: './input-program.component.html',
+  styleUrls: ['./input-program.component.sass']
+})
+export class InputProgramComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/program-generator/program-generator.module.ts
+++ src/app/program-generator/program-generator.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InputProgramComponent } from './input-program/input-program.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InputProgramComponent
+  ],
   imports: [
     CommonModule
   ]
