d629c8db131653699b11b88663ffaabc193ea249
John-son Hippolyte <Hippolyte.Johnson@ac-paris.fr>
2021-12-27 13:54:41 +0100

    add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputProgramComponent } from './src/app/program-generator/input-program/input-program.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputProgramComponent
   ],
   imports: [
     BrowserModule
