f213b562b36653c845f3f67ed3d00c8b20283ce9
John-son Hippolyte <Hippolyte.Johnson@ac-paris.fr>
2022-01-07 23:41:53 +0100

    add range date

--- src/app/program-generator/input-program/input-program.component.ts
+++ src/app/program-generator/input-program/input-program.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-input-program',
   templateUrl: './input-program.component.html',
-  styleUrls: ['./input-program.component.sass']
+  styleUrls: ['./input-program.component.sass'],
 })
 export class InputProgramComponent implements OnInit {
-
-  constructor() { }
+  form!: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  initForm() {
+    this.form = this.fb.group(
+      {
+        rangeDate: this.fb.group({
+          start: ['', Validators.required],
+          end: ['', Validators.required],
+        }),
+      },
+      {
+        validators: [],
+        updateOn: 'blur',
+      }
+    );
+  }
 }
--- src/app/program-generator/program-generator.module.ts
+++ src/app/program-generator/program-generator.module.ts
@@ -4,17 +4,21 @@ import { InputProgramComponent } from './input-program/input-program.component';
 import { GeneratorComponent } from './generator/generator.component';
 import { RouterModule } from '@angular/router';
 import { ProgramGeneratorRoutes } from './program-generator.routes';
-
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-  declarations: [
-    InputProgramComponent,
-    GeneratorComponent
-  ],
+  declarations: [InputProgramComponent, GeneratorComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(ProgramGeneratorRoutes)
-  ]
+    RouterModule.forChild(ProgramGeneratorRoutes),
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    ReactiveFormsModule
+  ],
+  providers: [FormBuilder],
 })
-export class ProgramGeneratorModule { }
+export class ProgramGeneratorModule {}
