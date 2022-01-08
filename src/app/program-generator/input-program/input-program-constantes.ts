997c5c6391000fc8e290b939b3bfedc942a3e60a
John-son Hippolyte <Hippolyte.Johnson@ac-paris.fr>
2022-01-08 23:21:09 +0100

    generate program dates

---
+++ src/app/program-generator/input-program/input-program-constantes.ts
@@ -0,0 +1,1 @@
+export const DATE_FORMAT_FR = 'dd/MM/yyyy';
--- src/app/program-generator/input-program/input-program.component.ts
+++ src/app/program-generator/input-program/input-program.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { addDays } from 'date-fns';
 import { SharedValidators } from 'src/app/shared/utils/shared-validators';
 
 @Component({
@@ -18,7 +19,7 @@ export class InputProgramComponent implements OnInit {
         'Vendredi',
         'Samedi',
     ];
-
+    programDates: Array<Date> = [];
     constructor(private fb: FormBuilder) {}
 
     ngOnInit(): void {
@@ -28,13 +29,16 @@ export class InputProgramComponent implements OnInit {
     initForm() {
         this.form = this.fb.group(
             {
-                participants: ['', Validators.required, SharedValidators.notBlank],
+                participants: [
+                    '',
+                    [Validators.required, SharedValidators.notBlank],
+                ],
                 rangeDate: this.fb.group({
                     start: ['', Validators.required],
                     end: ['', Validators.required],
                 }),
-                selectedDays: ['', Validators.required, SharedValidators.notBlank],
-                rolesNumber: [1, Validators.required]
+                selectedDays: ['', [Validators.required]],
+                rolesNumber: [1, Validators.required],
             },
             {
                 validators: [],
@@ -50,6 +54,32 @@ export class InputProgramComponent implements OnInit {
         );
         console.log('end : ' + this.form.get('rangeDate')?.get('end')?.value);
         console.log('participants : ' + this.form.get('participants')?.value);
+
+        this.initProgramDates();
+        console.log('programDates : ' + JSON.stringify(this.programDates));
     }
 
+    initProgramDates() {
+        console.log('form valid : ' + this.form.valid);
+        if (this.form.valid) {
+            this.programDates = [];
+            const selectedDays = this.form.get('selectedDays')
+                ?.value as Array<number>;
+            let currDate = new Date(
+                this.form.get('rangeDate')?.get('start')?.value
+            );
+            console.log(
+                'selectedDays : ' + selectedDays + ' currDate : ' + currDate
+            );
+            while (currDate <= this.form.get('rangeDate')?.get('end')?.value) {
+                console.log(
+                    'currDate : ' + currDate + ' getDay : ' + currDate.getDay()
+                );
+                if (selectedDays.includes(currDate.getDay())) {
+                    this.programDates.push(currDate);
+                }
+                currDate = addDays(currDate, 1);
+            }
+        }
+    }
 }
