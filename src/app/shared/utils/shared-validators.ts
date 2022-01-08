c3d89b7ae5d90f193e1a58e0a90b02dfa068eaf6
John-son Hippolyte <Hippolyte.Johnson@ac-paris.fr>
2022-01-08 23:20:34 +0100

    correct notBlank validator

--- src/app/shared/utils/shared-validators.ts
+++ src/app/shared/utils/shared-validators.ts
@@ -3,9 +3,9 @@ import { AbstractControl, ValidationErrors } from "@angular/forms";
 export class SharedValidators {
 
 static notBlank(control: AbstractControl) : ValidationErrors|null {
-    if (control?.value || (control?.value as string).trim().length){
+    if(control.value && (control.value as string).trim().length === 0) {
         return {notBlank: true}
     }
-    return null
+    return null;
 }
 }
