82ae7d59d3eeae144c90ea56d40b92ab1dbb21d4
John-son Hippolyte <Hippolyte.Johnson@ac-paris.fr>
2022-01-09 18:09:15 +0100

    init participants + clean

--- src/app/program-generator/input-program/input-program.component.ts
+++ src/app/program-generator/input-program/input-program.component.ts
@@ -2,7 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { addDays } from 'date-fns';
 import { SharedValidators } from 'src/app/shared/utils/shared-validators';
-import { END_FORM_CONTROL, PARTICIPANTS_FORM_CONTROL, RANGE_DATE_FORM_GROUP, SELECTED_DAYS_FORM_CONTROL, START_FORM_CONTROL } from './input-program-constantes';
+import {
+    END_FORM_CONTROL,
+    PARTICIPANTS_FORM_CONTROL,
+    RANGE_DATE_FORM_GROUP,
+    SELECTED_DAYS_FORM_CONTROL,
+    START_FORM_CONTROL,
+} from './input-program-constantes';
 import { InputProgramService } from './input-program.service';
 
 @Component({
@@ -22,6 +28,8 @@ export class InputProgramComponent implements OnInit {
         'Samedi',
     ];
     programDates: Array<Date> = [];
+    participants: Array<string> = [];
+
     constructor(
         private fb: FormBuilder,
         private inputProgramService: InputProgramService
@@ -53,41 +61,36 @@ export class InputProgramComponent implements OnInit {
     }
 
     onValidateForm1() {
-        // console.log('rangeDate : '+JSON.stringify(this.form.get('rangeDate')))
-        console.log(
-            'start : ' + this.form.get(RANGE_DATE_FORM_GROUP)?.get(START_FORM_CONTROL)?.value
-        );
-        console.log('end : ' + this.form.get(RANGE_DATE_FORM_GROUP)?.get(END_FORM_CONTROL)?.value);
-        console.log('participants : ' + this.form.get(PARTICIPANTS_FORM_CONTROL)?.value);
-
-        this.initProgramDates();
-        console.log('programDates : ');
-        this.programDates.forEach((date) =>
-            console.log(this.inputProgramService.toDateFr(date))
-        );
+        if (this.form.valid) {
+            this.initProgramDates();
+            this.initParticipants();
+            console.log('partici prop : '+JSON.stringify(this.participants)); 
+        }
     }
 
     initProgramDates() {
-        console.log('form valid : ' + this.form.valid);
-        if (this.form.valid) {
-            this.programDates = [];
-            const selectedDays = this.form.get(SELECTED_DAYS_FORM_CONTROL)
-                ?.value as Array<number>;
-            let currDate = new Date(
-                this.form.get(RANGE_DATE_FORM_GROUP)?.get(START_FORM_CONTROL)?.value
-            );
-            console.log(
-                'selectedDays : ' + selectedDays + ' currDate : ' + currDate
-            );
-            while (currDate <= this.form.get(RANGE_DATE_FORM_GROUP)?.get(END_FORM_CONTROL)?.value) {
-                console.log(
-                    'currDate : ' + currDate + ' getDay : ' + currDate.getDay()
-                );
-                if (selectedDays.includes(currDate.getDay())) {
-                    this.programDates.push(currDate);
-                }
-                currDate = addDays(currDate, 1);
+        this.programDates = [];
+        const selectedDays = this.form.get(SELECTED_DAYS_FORM_CONTROL)
+            ?.value as Array<number>;
+        let currDate = new Date(
+            this.form.get(RANGE_DATE_FORM_GROUP)?.get(START_FORM_CONTROL)?.value
+        );
+        while (
+            currDate <=
+            this.form.get(RANGE_DATE_FORM_GROUP)?.get(END_FORM_CONTROL)?.value
+        ) {
+            if (selectedDays.includes(currDate.getDay())) {
+                this.programDates.push(currDate);
             }
+            currDate = addDays(currDate, 1);
         }
+    }
+
+    initParticipants() {
+        this.participants = (
+            this.form.get(PARTICIPANTS_FORM_CONTROL)?.value as string
+        )
+            .split('\n')
+            .map((participant) => participant.trim()).filter(s => s.length>0);
     }
 }
