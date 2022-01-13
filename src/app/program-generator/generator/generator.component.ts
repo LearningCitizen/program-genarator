40baf538eebbb38a1ffe0e9fbea376cad22c3826
John-son Hippolyte <Hippolyte.Johnson@ac-paris.fr>
2022-01-13 22:14:33 +0100

    call generate onInit

--- src/app/program-generator/generator/generator.component.ts
+++ src/app/program-generator/generator/generator.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { InputProgramGenerator } from '../input-program/input-program-constantes';
 import { InputProgramService } from '../input-program/input-program.service';
 
 @Component({
-  selector: 'app-generator',
-  templateUrl: './generator.component.html',
-  styleUrls: ['./generator.component.sass']
+    selector: 'app-generator',
+    templateUrl: './generator.component.html',
+    styleUrls: ['./generator.component.sass'],
 })
 export class GeneratorComponent implements OnInit {
+    programGenerated: any;
+    inputProgram: InputProgramGenerator | undefined = undefined;
+    constructor(private inputProgramService: InputProgramService) {}
 
-  programGenerated : any
-  constructor(private inputProgramService: InputProgramService) { }
+    ngOnInit(): void {
+        this.inputProgramService.inputProgramGenerator$.subscribe((input) => {
+            this.inputProgram = input;
+            this.generateProgram();
+        });
+    }
 
-  ngOnInit(): void {
-  }
-
+    generateProgram() {}
 }
