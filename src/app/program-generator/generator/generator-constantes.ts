5556fc33aadcaa4ef37e1460fb1adcacde5b976e
John-son Hippolyte <Hippolyte.Johnson@ac-paris.fr>
2022-01-15 21:51:57 +0100

    compute participants with minimal assignments

---
+++ src/app/program-generator/generator/generator-constantes.ts
@@ -0,0 +1,6 @@
+/* Columns */
+export const DATE_COLUMN = 'date';
+export const ROLE_COLUMN = 'role';
+
+/* Others */
+export const PARTICIPANTS_UNAVAILABLE = 'Participants indisponibles';
--- src/app/program-generator/generator/generator.component.ts
+++ src/app/program-generator/generator/generator.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { InputProgramGenerator } from '../input-program/input-program-constantes';
 import { InputProgramService } from '../input-program/input-program.service';
+import {
+    DATE_COLUMN,
+    PARTICIPANTS_UNAVAILABLE,
+    ROLE_COLUMN,
+} from './generator-constantes';
 
 type ProgramAssignment = {
     date: Date;
-    roles: string[]
+    roles: string[];
 };
 
 @Component({
@@ -16,7 +21,7 @@ export class GeneratorComponent implements OnInit {
     programGenerated: ProgramAssignment[] = [];
     inputProgram: InputProgramGenerator | undefined = undefined;
     participantsCounter: Map<string, number> = new Map<string, number>();
-    columnsToDisplay = ['date', 'role'];
+    columnsToDisplay = [DATE_COLUMN, ROLE_COLUMN];
 
     constructor(private inputProgramService: InputProgramService) {}
 
@@ -30,12 +35,20 @@ export class GeneratorComponent implements OnInit {
     generateProgram() {
         this.initParticipantsCounter();
         let rolesForCurrentDate: string[];
+        let participantForCurrentDate: string[];
         this.inputProgram?.pgmDates.forEach((pgmDate, i) => {
             rolesForCurrentDate = [];
+            participantForCurrentDate = [];
             Array.from({ length: this.inputProgram?.roles[i] ?? 0 }).forEach(
-                (_, i2) => {rolesForCurrentDate.push(`Rôle ${i2+1}`)}
+                (_, i2) => {
+                    rolesForCurrentDate.push(`Rôle ${i2 + 1}`);
+                    participantForCurrentDate.push(this.getParticipantWithMinimumAssignments(i, i2))
+                }
             );
-            this.programGenerated.push({ date: pgmDate, roles: rolesForCurrentDate });
+            this.programGenerated.push({
+                date: pgmDate,
+                roles: rolesForCurrentDate,
+            });
         });
     }
 
@@ -43,5 +56,43 @@ export class GeneratorComponent implements OnInit {
         this.inputProgram?.participants.forEach((par) =>
             this.participantsCounter.set(par, 0)
         );
+    }
+
+    getParticipantWithMinimumAssignments(
+        index: number,
+        index2: number
+    ): string {
+        let minParticipant = PARTICIPANTS_UNAVAILABLE;
+        if (this.inputProgram) {
+            let minAssignments = Number.MAX_VALUE;
+            let parAssignments: number;
+            let participantsAvailable = this.getAvailableParticipants(
+                this.inputProgram.unavailabilitiy[index][index2]
+            );
+            /* Looking for participants available with min assignments */
+            participantsAvailable.forEach((parAvail) => {
+                parAssignments =
+                    this.participantsCounter.get(parAvail) ?? Number.MAX_VALUE;
+                if (parAssignments < minAssignments) {
+                    minParticipant = parAvail;
+                    minAssignments = parAssignments;
+                }
+            });
+            if (minParticipant !== PARTICIPANTS_UNAVAILABLE) {
+                const nbAssignments =
+                    this.participantsCounter.get(minParticipant) ?? 0;
+                this.participantsCounter.set(minParticipant, nbAssignments + 1);
+            }
+            this.inputProgram?.unavailabilitiy[index][index2];
+        }
+        return minParticipant;
+    }
+
+    getAvailableParticipants(unavailableParticipants: string[]): string[] {
+        return this.inputProgram?.participants
+            ? this.inputProgram?.participants.filter(
+                  (par) => !unavailableParticipants.includes(par)
+              )
+            : [];
     }
 }
